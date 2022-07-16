aa34042a0619731c4df5f419479cd9e7ee7b9ec5
fanyone <943211298@qq.com>
2022-07-17 00:08:32 +0800

    merge EnemySpawnSystem with EnemyManager

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyManager {
+public class EnemyManager : MonoBehaviour
+{
     public HashSet<GameObject> mEnemies;
-    public EnemyManager() {
-        mEnemies = new HashSet<GameObject>();
+    /* Spawn Parts */
+    [System.Serializable]
+    public struct EnemyInfo
+    {
+        public GameObject enemyPrefab;
+        public string pathName;
+        public int totalSpawn;
+        public float cooldownTime;
     }
+    [System.Serializable]
+    public struct PhaseInfo
+    {
+        public EnemyInfo[] enemies;
+    }
+    public PhaseInfo[] phases;
+
+    private float[] lastSpawnTime = null;
+    private int[] numSpawn = null;
+    private EnemyInfo[] enemyToSpawn = null;
+    private bool spawning = false;
+    private int numPhaseInfo;
+    private int numEnemyInfo;
+
     public void AddEnemy(GameObject enemy) {
         mEnemies.Add(enemy);
     }
@@ -28,5 +49,65 @@ public class EnemyManager {
             }
         }
         return closest;
+    }
+    private void Start()
+    {
+        mEnemies = new HashSet<GameObject>();
+        numPhaseInfo = phases.Length;
+        // test spawning
+        StartPhase(1);
+    }
+    private void Update()
+    {
+        if (spawning) Spawn();
+    }
+    public void StartPhase(int index)
+    {
+        Debug.Assert(index >= 0 && index < numPhaseInfo);
+
+        enemyToSpawn = phases[index].enemies;
+        numEnemyInfo = enemyToSpawn.Length;
+
+        if (numEnemyInfo > 0)
+        {
+            lastSpawnTime = new float[numEnemyInfo];
+            numSpawn = new int[numEnemyInfo];
+            float current = Time.time;
+            for (int i = 0; i < numEnemyInfo; i++)
+            {
+                lastSpawnTime[i] = current;
+                numSpawn[i] = 0;
+            }
+
+            spawning = true;
+        }
+    }
+    private void Spawn()
+    {
+        Debug.Assert(numSpawn != null && lastSpawnTime != null);
+
+        spawning = false;
+
+        for (int i = 0; i < enemyToSpawn.Length; i++)
+        {
+            // check number
+            if (numSpawn[i] < enemyToSpawn[i].totalSpawn)
+            {
+                spawning = true;
+                // check cooldown
+                float current = Time.time;
+                if (current - lastSpawnTime[i] >= enemyToSpawn[i].cooldownTime * Time.smoothDeltaTime)
+                {
+                    Debug.Log(current);
+                    GameObject e = Instantiate(enemyToSpawn[i].enemyPrefab);
+                    EnemyBehavior eb = e.GetComponent<EnemyBehavior>();
+                    Debug.Assert(eb != null);
+                    eb.SetPath(enemyToSpawn[i].pathName);
+
+                    lastSpawnTime[i] = current;
+                    numSpawn[i]++;
+                }
+            }
+        }
     }
 }
