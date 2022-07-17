89de806c4d22d3fd1cc4a6271c4511b34f5f809f
ZRJ <zhangrujia@sjtu.edu.cn>
2022-07-18 00:16:48 +0800

    enemy shoot player

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -7,7 +7,6 @@ public class EnemyBehavior : MonoBehaviour
     private Animator _Anim;
     private enum AnimState { Idle, Down, Up, Left, Right }
 
-    public GameObject bulletPrefab;
     public int maxHP = 5;
     public float pathSpeed = 1.0f;
 
@@ -16,22 +15,20 @@ public class EnemyBehavior : MonoBehaviour
     public void SetPath(string n) { pathName = n; }
     private int currentHP;
     private float pathDistance;
-    private float fireForce = 20.0f;
     // Start is called before the first frame update
 
-    private void Awake()
+    public void Awake()
     {
         _Anim = GetComponentInChildren<Animator>();
         _LastPoint = transform.position;
         _RadiusC.enabled = false;
     }
 
-    void Start()
+    public void Start()
     {
         pathSystem = GameManager.sTheGlobalBehavior.mPathSystem;
         GameManager.sTheGlobalBehavior.mEnemyManager.AddEnemy(gameObject);
         Debug.Assert(pathSystem != null);
-        Debug.Assert(bulletPrefab != null);
         Debug.Assert(maxHP > 0);
 
         currentHP = maxHP;
@@ -39,16 +36,10 @@ public class EnemyBehavior : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         UpdatePosition();
         UpdateAnim();
-    }
-
-    private void Fire()
-    {
-        GameObject b = Instantiate(bulletPrefab, transform.position, transform.rotation);
-        b.GetComponent<Rigidbody2D>().AddForce(transform.up * fireForce, ForceMode2D.Impulse);
     }
 
     private void UpdatePosition()
--- Assets/Scripts/Enemy/PlayerKiller.cs
+++ Assets/Scripts/Enemy/PlayerKiller.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerKiller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Bullet bulletPrefab;
+    public float mShootCooldown = 1.0f;
+    private float mLastShotTime = 0f;
+    public float radius = 0f;
+    void Start() {
+        if (radius == 0) radius = gameObject.GetComponent<EnemyBehavior>().GetRadius();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Fire(Vector3 target) {
+        Bullet b = Instantiate(bulletPrefab, transform.position,transform.rotation);
+        b._Force = 5f;
+        Vector2 direction = target - transform.position;
+        b.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f);
+        b.transform.position = b.transform.position + b.transform.up * radius;
+        b.Fire();
+    }
+    void Update() {
+        if (Time.time - mLastShotTime < mShootCooldown)
+            return;
+        if (GameManager.sTheGlobalBehavior.mHero != null) {
+            Fire(GameManager.sTheGlobalBehavior.mHero.transform.position);
+            mLastShotTime = Time.time;
+        }
     }
 }
