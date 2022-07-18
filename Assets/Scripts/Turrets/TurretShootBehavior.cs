a3d4e468cde27a698c195732182256ba25b75741
ZRJ <zhangrujia@sjtu.edu.cn>
2022-07-18 19:53:03 +0800

    Turret hp

--- Assets/Scripts/Turrets/TurretShootBehavior.cs
+++ Assets/Scripts/Turrets/TurretShootBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
+public class TurretShootBehavior : MonoBehaviour
 {
     public FriendBulletBehavior _BulletPre;
     public Transform _Gun;
