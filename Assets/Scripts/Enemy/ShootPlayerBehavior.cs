393047db1a994fa000ade6f3337b1d76185cb086
ZRJ <zhangrujia@sjtu.edu.cn>
2022-07-18 19:01:43 +0800

    Fix scene and ShootPlayerBehavior

--- Assets/Scripts/Enemy/ShootPlayerBehavior.cs
+++ Assets/Scripts/Enemy/ShootPlayerBehavior.cs
@@ -2,21 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerKiller : MonoBehaviour
+public class ShootPlayerBehavior : MonoBehaviour
 {
-    public Bullet bulletPrefab;
+    public EnemyBulletBehavior bulletPrefab;
     public float mShootCooldown = 1.0f;
     private float mLastShotTime = 0f;
-    public float radius = 0f;
+    public float mShootFromRadius = 0.7f;
     void Start() {
-        if (radius == 0) radius = gameObject.GetComponent<EnemyBehavior>().GetRadius();
     }
     void Fire(Vector3 target) {
-        Bullet b = Instantiate(bulletPrefab, transform.position,transform.rotation);
-        b._Force = 5f;
+        EnemyBulletBehavior b = Instantiate(bulletPrefab, transform.position,transform.rotation);
         Vector2 direction = target - transform.position;
         b.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f);
-        b.transform.position = b.transform.position + b.transform.up * radius;
+        b.transform.position = b.transform.position + b.transform.up * mShootFromRadius;
         b.Fire();
     }
     void Update() {
