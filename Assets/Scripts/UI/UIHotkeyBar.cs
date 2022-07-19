959735b1638ed932e34651de595a0b88c39ff44b
Ian Chong <ianchong000@gmail.com>
2022-07-19 11:44:42 +0800

    hotbar v1.1

--- Assets/Scripts/UI/UIHotkeyBar.cs
+++ Assets/Scripts/UI/UIHotkeyBar.cs
@@ -5,14 +5,28 @@ using UnityEngine.UI;
 
 public class UIHotkeyBar : MonoBehaviour
 {
-    public void Activate(int index) {
+    public Sprite emptySprite;
+
+    public void SetItemIcon(int index, Sprite icon)
+    {
+        if (icon == null) icon = emptySprite;
+        transform.Find("slot"+index).Find("Icon").GetComponent<Image>().sprite = icon;
+    }
+    public void SetItemCount(int index, int count)
+    {
+
+    }
+    public void Activate(int index) 
+    {
         Transform border = transform.Find("slot"+index).Find("border");
         border.GetComponent<Image>().enabled = true;
     }
-    public void Deactivate(int index) {
+    public void Deactivate(int index) 
+    {
         Transform border = transform.Find("slot"+index).Find("border");
         border.GetComponent<Image>().enabled = false;
     }
+    //TO BE IMPLEMENTED
     public void StartCooldDown(float time, int index)
     {
 
