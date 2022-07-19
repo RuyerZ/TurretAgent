335778eba21b6b126eb0463c585f52ee198c5994
Mengqi97 <61009324+Mengqi97@users.noreply.github.com>
2022-07-19 17:09:40 +0800

    cai_menu

---
+++ Assets/Scripts/Menu/MenuScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+
+    //  开始按钮
+    public void OnPlayBtn()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    //  退出按钮
+    public void OnQuitBtn()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;//编辑器模式下游戏退出
+#else
+        Application.Quit();//游戏发布后游戏退出
+#endif
+    }
+
+}
