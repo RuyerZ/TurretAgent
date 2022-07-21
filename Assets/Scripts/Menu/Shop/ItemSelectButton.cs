52531e5fba12d1d88a9deb6ef69e33d8d3dc474a
fanyone <943211298@qq.com>
2022-07-21 16:45:53 +0800

    Shop Menu Update

--- Assets/Scripts/Menu/Shop/ItemSelectButton.cs
+++ Assets/Scripts/Menu/Shop/ItemSelectButton.cs
@@ -7,11 +7,14 @@ public class ItemSelectButton : MonoBehaviour
 {
     public GameObject mSelectedEffect;
     private int mItemIndex;
+    private bool mSold = false;
     private ShopMenu mShopMenu;
+    private Button mButton;
     // Start is called before the first frame update
     void Start()
     {
         mShopMenu = GetComponentInParent<ShopMenu>();
+        mButton = GetComponentInChildren<Button>();
     }
 
     // Update is called once per frame
@@ -21,15 +24,23 @@ public class ItemSelectButton : MonoBehaviour
     }
 
     public int Index { get { return mItemIndex; } set { mItemIndex = value; } }
+    public bool IsSold { get { return mSold; } }
 
     public void SetWindowActive(bool active)
     {
         mSelectedEffect.SetActive(active);
     }
 
+    public void SetSold()
+    {
+        SetWindowActive(false);
+        mButton.interactable = false;
+        mSold = true;
+    }
+
     public void OnClick()
     {
-        if (mShopMenu != null)
+        if (!mSold)
             mShopMenu.Select(mItemIndex);
     }
 }
